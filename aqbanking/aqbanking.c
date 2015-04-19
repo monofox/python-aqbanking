4937f6f74b433f59b04111a7ce95530332d0e944
Lukas Schreiner <lukas.schreiner@gmail.com>
2015-04-19 21:35:10 +0200

    The userid is not very beautiful.

--- aqbanking/aqbanking.c
+++ aqbanking/aqbanking.c
@@ -7,10 +7,30 @@
 #include "unicodeobject.h"
 #include "structmember.h"
 #include <aqbanking/banking.h>
+#include <aqbanking/jobgetbalance.h>
 #include <gwenhywfar/cgui.h>
 
+/**
+ * Here are some exceptions.
+ */
 static PyObject *AqBankingInitializeError;
 static PyObject *AqBankingDeInitializeError;
+static PyObject *AccountNotFound;
+static PyObject *ExecutionFailed;
+
+/**
+ * Here are some callback variables.
+ */
+static PyObject *callbackLog = NULL;
+static PyObject *callbackMsgBox = NULL;
+static PyObject *callbackPassword = NULL;
+static PyObject *userId = NULL;
+
+/**
+ * AqBanking
+ */
+AB_BANKING *ab;
+GWEN_GUI *gui;
 
 typedef struct {
 	PyObject_HEAD
@@ -204,28 +224,17 @@ static PyTypeObject aqbanking_AccountType = {
 
 static PyObject * aqbanking_listacc(PyObject *self, PyObject *args)
 {
-	AB_BANKING *ab;
-	AB_ACCOUNT_LIST2 *accs;
 	int rv;
-	GWEN_GUI *gui;
+	AB_ACCOUNT_LIST2 *accs;
 	// List of accounts => to return.
 	PyObject *accountList;
 	aqbanking_Account *account;
 	accountList = PyList_New(0);
 
-	gui=GWEN_Gui_CGui_new();
-	GWEN_Gui_SetGui(gui);
-
-	ab = AB_Banking_new("python-aqbanking", 0, 0);
-	rv = AB_Banking_Init(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not initialize (%d).", rv));
-		return NULL;
-	}
-
-	rv = AB_Banking_OnlineInit(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do onlineinit. (%d).", rv));
+	// Initialize aqbanking.
+	rv = AB_create();
+	if (rv > 0)
+	{
 		return NULL;
 	}
 
@@ -292,56 +301,44 @@ static PyObject * aqbanking_listacc(PyObject *self, PyObject *args)
 		AB_Account_List2_free(accs);
 	}
 
-
-	rv=AB_Banking_OnlineFini(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do online deinit. (%d).", rv));
+	// Exit aqbanking.
+	rv = AB_free();
+	if (rv > 0)
+	{
 		Py_DECREF(account);
 		Py_DECREF(accountList);
 		return NULL;
 	}
 
-	rv=AB_Banking_Fini(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do deinit. (%d).", rv));
-		Py_DECREF(account);
-		Py_DECREF(accountList);
-		return NULL;
-	}
-	AB_Banking_free(ab);
 	return accountList;
 }
 
 static PyObject * aqbanking_chkiban(PyObject *self, PyObject *args)
 {
-	AB_BANKING *ab;
 	AB_BANKINFO_CHECKRESULT res;
 	int rv;
-	GWEN_GUI *gui;
 	// List of accounts => to return.
 	const char *iban;
 
 	if (!PyArg_ParseTuple(args, "s", &iban))
 		return NULL;
-
-	gui=GWEN_Gui_CGui_new();
-	GWEN_Gui_SetGui(gui);
-
-	ab = AB_Banking_new("python-aqbanking", 0, 0);
-	rv = AB_Banking_Init(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not initialize (%d).", rv));
+	
+	// Initialize aqbanking.
+	rv = AB_create();
+	if (rv > 0)
+	{
 		return NULL;
 	}
 
 	res = AB_Banking_CheckIban(iban);
 
-	rv = AB_Banking_Fini(ab);
-	if (rv) {
-		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do deinit. (%d).", rv));
+	// Exit aqbanking.
+	rv = AB_free();
+	if (rv > 0)
+	{
 		return NULL;
 	}
-	AB_Banking_free(ab);
+
 	if (res == 0) {
 		return PyBool_FromLong(1);
 	} else {
@@ -349,9 +346,267 @@ static PyObject * aqbanking_chkiban(PyObject *self, PyObject *args)
 	}
 }
 
+static PyObject *aqbanking_balance(PyObject *self, PyObject *args, PyObject *keywds)
+{
+	const AB_ACCOUNT_STATUS * status;
+	const AB_BALANCE * bal;
+	const AB_VALUE *v = 0;
+	int rv;
+	double balance;
+	//const char *currency;
+	const char *bank_code;
+	const char *account_no;
+	AB_ACCOUNT *a;
+	AB_USER *u = 0;
+	AB_JOB *job = 0;
+	AB_JOB_LIST2 *jl = 0;
+	AB_IMEXPORTER_CONTEXT *ctx = 0;
+	AB_IMEXPORTER_ACCOUNTINFO *ai;
+	// Return:
+	PyObject *result;
+
+	static char *kwlist[] = {"account", "bank", NULL};
+
+	if (!PyArg_ParseTupleAndKeywords(args, keywds, "ss", kwlist, &account_no, &bank_code))
+		return NULL;
+
+	// Initialize aqbanking.
+	rv = AB_create();
+	if (rv > 0)
+	{
+		return NULL;
+	}
+
+	// Let us find the account!
+	a = AB_Banking_GetAccountByCodeAndNumber(ab, bank_code, account_no);
+	if (!a)
+	{
+		PyErr_SetString(AccountNotFound, "Could not find the given account!");
+		return NULL;
+	}
+
+	u = AB_Account_GetFirstUser(a);
+
+	// Create job and execute it.
+	ctx = AB_ImExporterContext_new();
+	jl = AB_Job_List2_new();
+	job = AB_JobGetBalance_new(a);
+	AB_Job_List2_PushBack(jl, job);
+	rv = AB_Banking_ExecuteJobs(ab, jl, ctx);
+
+	if (rv)
+	{
+		PyErr_SetString(ExecutionFailed, "Could not get the balance!");
+		return NULL;
+	}
+
+	// With success. No process the result.
+	ai = AB_ImExporterContext_GetFirstAccountInfo (ctx);
+	status = AB_ImExporterAccountInfo_GetFirstAccountStatus (ai);
+	bal = AB_AccountStatus_GetBookedBalance (status);
+	v = AB_Balance_GetValue (bal);
+	balance = AB_Value_GetValueAsDouble(v);
+	//currency = AB_Value_GetCurrency(v);
+
+	// Free jobs.
+	AB_Job_List2_free(jl);
+	AB_ImExporterContext_free(ctx);
+
+	// Exit aqbanking.
+	rv = AB_free();
+	if (rv > 0)
+	{
+		return NULL;
+	}
+
+	return Py_BuildValue("(d,s)", balance, "EUR");
+}
+
+/**
+ * Gwen Gui progress log callback (which will then send to python callback!)
+ */
+int zProgressLog(GWEN_GUI *gui, uint32_t id, GWEN_LOGGER_LEVEL level, const char *text) 
+{
+	PyObject *arglist = Py_BuildValue("ss", userId, text);
+	PyObject_CallObject(callbackLog, arglist);
+	Py_DECREF(arglist);
+
+	return 0;
+}
+
+/**
+ * Gwen Gui message box callback (which will then send to python callback!)
+ */
+int zMessageBox(GWEN_GUI *gui, uint32_t flags,  const char *title,  const char *text,  const char *b1,  const char *b2,  const char *b3,  uint32_t guiid)
+{
+	long retCode;
+	PyObject *result;
+	PyObject *arglist = Py_BuildValue("ssssss", userId, title, text, b1, b2, b3);
+	result = PyObject_CallObject(callbackMsgBox, arglist);
+	Py_DECREF(arglist);
+
+	// So we need a result! So now lets check it.
+	if (result == NULL)
+		return 1;
+
+	// Now convert the object to int.
+	retCode = PyLong_AsLong(result);
+	if (retCode == -1 && PyErr_Occurred())
+	{
+		return -1;
+	}
+
+	return (int)retCode;
+}
+
+/**
+ * Gwen GUI passwort callback.
+ * @param  gui    [description]
+ * @param  flags  [description]
+ * @param  token  [description]
+ * @param  title  [description]
+ * @param  text   [description]
+ * @param  buffer [description]
+ * @param  minLen [description]
+ * @param  maxLen [description]
+ * @param  guiid  [description]
+ * @return        [description]
+ */
+int zPasswordFn(GWEN_GUI *gui, uint32_t flags, const char *token, const char *title, const char *text, char *buffer, int minLen, int maxLen, uint32_t guiid)
+{
+	const char *passwordPy;
+	PyObject *result;
+	PyObject *arglist = Py_BuildValue("sIsssii", userId, flags, token, title, text, minLen, maxLen);
+	result = PyObject_CallObject(callbackPassword, arglist);
+	Py_DECREF(arglist);
+
+	// So we need a result! So now lets check it.
+	if (result == NULL) {
+		fprintf(stderr, "%s", "No password returned!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+		PyErr_Print(); 
+		return 1;
+	} else {
+		// Now convert the object to int.
+		passwordPy = PyUnicode_AsUTF8(result);
+		strcpy(buffer, passwordPy);
+		Py_DECREF(result);
+	}
+
+	return 0;
+}
+
+/**
+ * Return always: yes mam!
+ */
+int zCheckCert(GWEN_GUI *gui, const GWEN_SSLCERTDESCR *cd, GWEN_SYNCIO *sio, uint32_t guiid) 
+{
+	// FIXME: implement at least some basic checks => or ask user via callback!
+	return 0;
+}
+
+static PyObject *aqbanking_set_callbackLog(PyObject *dummy, PyObject *args)
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O:set_callbackLog", &temp)) {
+        if (!PyCallable_Check(temp)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);         /* Add a reference to new callback */
+        Py_XDECREF(callbackLog);  /* Dispose of previous callback */
+        callbackLog = temp;       /* Remember new callback */
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+static PyObject *aqbanking_set_callbackMsgBox(PyObject *dummy, PyObject *args)
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O:set_callbackMsgBox", &temp)) {
+        if (!PyCallable_Check(temp)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);         /* Add a reference to new callback */
+        Py_XDECREF(callbackMsgBox);  /* Dispose of previous callback */
+        callbackMsgBox = temp;       /* Remember new callback */
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+static PyObject *aqbanking_set_callbackPassword(PyObject *dummy, PyObject *args)
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O:set_callbackPassword", &temp)) {
+        if (!PyCallable_Check(temp)) {
+            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+            return NULL;
+        }
+        Py_XINCREF(temp);         /* Add a reference to new callback */
+        Py_XDECREF(callbackPassword);  /* Dispose of previous callback */
+        callbackPassword = temp;       /* Remember new callback */
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+static PyObject *aqbanking_set_user_id(PyObject *dummy, PyObject *args)
+{
+    PyObject *result = NULL;
+    PyObject *temp;
+
+    if (PyArg_ParseTuple(args, "O:set_user_id", &temp)) {
+        Py_XINCREF(temp);         /* Add a reference to new callback */
+        Py_XDECREF(userId);  /* Dispose of previous callback */
+        userId = temp;       /* Remember new callback */
+        /* Boilerplate to return "None" */
+        Py_INCREF(Py_None);
+        result = Py_None;
+    }
+    return result;
+}
+
+/**
+ * TODO: some kind of testing!!!!
+ */
+static PyObject *aqbanking_hello(PyObject *self, PyObject *args)
+{
+	// Time to call our callback!
+	PyObject *result;
+	PyObject *arglist = Py_BuildValue("(s)", "Hello World!!!!");
+	result = PyObject_CallObject(callbackLog, arglist);
+	Py_DECREF(arglist);
+
+	// So we need a result! So now lets check it.
+	if (result == NULL)
+		return NULL;
+
+	return result;
+}
+
 static PyMethodDef AqBankingMethods[] = {
 	{"listacc", aqbanking_listacc, METH_VARARGS, "Get a list of accounts"},
+	{"balance", (PyCFunction)aqbanking_balance, METH_VARARGS | METH_KEYWORDS, "Get the balance of a specific account."},
 	{"chkiban", aqbanking_chkiban, METH_VARARGS, "Validates an IBAN"},
+	{"set_callbackLog", aqbanking_set_callbackLog, METH_VARARGS, "Adds a callback for the log output."},
+	{"set_callbackMsgBox", aqbanking_set_callbackMsgBox, METH_VARARGS, "Adds a callback for the log output."},
+	{"set_callbackPassword", aqbanking_set_callbackPassword, METH_VARARGS, "Adds a callback to retrieve the password (pin)."},
+	{"set_user_id", aqbanking_set_user_id, METH_VARARGS, "Set the user id of the session."},
+	{"hello", aqbanking_hello, METH_VARARGS, "Triggers a hello world to callback."},
 
 	{NULL, NULL, 0, NULL}
 };
@@ -364,30 +619,89 @@ static struct PyModuleDef aqbankingmodule = {
 	AqBankingMethods
 };
 
+int AB_create() {
+
+        int rv = 0;
+
+        //Initialisierungen GWEN
+        //GWEN_Logger_SetLevel(0, GWEN_LoggerLevel_Verbous);
+        gui = GWEN_Gui_CGui_new();
+        GWEN_Gui_SetGui(gui);
+
+        // Initialisierungen AB
+        ab = AB_Banking_new("python-aqbanking", 0, AB_BANKING_EXTENSION_NONE);
+        rv = AB_Banking_Init(ab);
+        if (rv) {
+                PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not initialize (%d).", rv));
+                return 2;
+        }
+        rv = AB_Banking_OnlineInit(ab);
+        if (rv) {
+        		PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do online initialize (%d).", rv));
+                return 2;
+        }
+        
+        //Setzen der Call-backs
+        GWEN_Gui_SetProgressLogFn(gui, zProgressLog);
+        GWEN_Gui_SetCheckCertFn(gui, zCheckCert);
+        GWEN_Gui_SetMessageBoxFn(gui, zMessageBox);
+        GWEN_Gui_SetGetPasswordFn(gui, zPasswordFn);
+
+        return 0;
+
+}
+
+int AB_free() {
+        int rv = 0;
+
+        rv = AB_Banking_OnlineFini(ab);
+        if (rv) 
+        {
+                PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do online deinit. (%d).", rv));
+                return 3;
+        }
+
+        rv = AB_Banking_Fini(ab);
+        if (rv) 
+        {
+                PyErr_SetObject(AqBankingInitializeError, PyUnicode_FromFormat("Could not do deinit. (%d).", rv));
+                return 3;
+        }
+
+        AB_Banking_free(ab);
+        return 0;
+}
+
 PyMODINIT_FUNC
 PyInit_aqbanking(void)
 {
 	PyObject *m;
 
-	/* First initialize the types */
+	// First initialize the types
 	aqbanking_AccountType.tp_new = PyType_GenericNew;
 	if (PyType_Ready(&aqbanking_AccountType) < 0)
 		return NULL;
 
-	/* And then the module. */
+	// And then the module. 
 	m = PyModule_Create(&aqbankingmodule);
 	if (m == NULL)
 		return NULL;
 
-	/* Exceptions */
+	// Exceptions 
 	AqBankingInitializeError = PyErr_NewException("aqbanking.AqBankingInitializeError", NULL, NULL);
 	Py_INCREF(AqBankingInitializeError);
 	PyModule_AddObject(m, "AqBankingInitializeError", AqBankingInitializeError);
 	AqBankingDeInitializeError = PyErr_NewException("aqbanking.AqBankingDeInitializeError", NULL, NULL);
 	Py_INCREF(AqBankingDeInitializeError);
 	PyModule_AddObject(m, "AqBankingDeInitializeError", AqBankingDeInitializeError);
+	AccountNotFound = PyErr_NewException("aqbanking.AccountNotFound", NULL, NULL);
+	Py_INCREF(AccountNotFound);
+	PyModule_AddObject(m, "AccountNotFound", AccountNotFound);
+	ExecutionFailed = PyErr_NewException("aqbanking.ExecutionFailed", NULL, NULL);
+	Py_INCREF(ExecutionFailed);
+	PyModule_AddObject(m, "ExecutionFailed", ExecutionFailed);
 
-	/* Some types */
+	// Some types
 	Py_INCREF(&aqbanking_AccountType);
 	PyModule_AddObject(m, "Account", (PyObject *)&aqbanking_AccountType);
 
